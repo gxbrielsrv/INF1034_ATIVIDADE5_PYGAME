4d2b97ee63218df682bf1c1b81ec24acc363d572
Gabriel Rodrigues <gabriel.s.r.br.gr@gmail.com>
2026-04-06 17:20:34 -0300

    finish

--- main.py
+++ main.py
@@ -2,19 +2,30 @@ from pygame import *
 import sys 
 init()
 
+
+#imagens
 newton_img = image.load("newton.png")
 newton_img = transform.scale(newton_img,(250,250))
+maca_img = image.load("maca.png")
+maca_img = transform.scale(maca_img,(50,50))
 
-newton_font = font.Font("fonte_newton", 30, True, False)
+#texto
+newton_font = font.Font("contrast.ttf", 30)
+newton_text = newton_font.render("I am Newton", True, (0,0,0))
 
-# newton_music.load("newton_music.mp3")
-# mixer_music.play(-1)
-
-
+#musica
+mixer_music.load("newton_music.mp3")
+mixer_music.play(-1)
+music = mixer.Sound("newton_music.mp3")
+music.set_volume(0.1)
 
 
 window = display.set_mode((1280,720))
 window.fill((151, 209, 250))
+
+#nuvem andando
+nuvem_x = 800
+velocidade = 2
 
 
 while True:
@@ -23,13 +34,20 @@ while True:
             quit()
             sys.exit()
 
+    
+    window.fill((151, 209, 250))
+    nuvem_x += velocidade
+    if nuvem_x > 1280:
+        nuvem_x = -200
+    
+
     #Desenhar aqui:
     draw.rect(window,(72, 157, 37),(0,580,1280,220))
-    draw.circle(window,(255, 255, 255,), (800, 100), 50)
-    draw.circle(window,(255, 255, 255,), (865, 100), 50)
-    draw.circle(window,(255, 255, 255,), (930, 100), 50)
-    draw.circle(window,(255, 255, 255,), (995, 100), 50)
-    draw.rect(window,(100, 100, 100),(250, 330, 250,250))
+    draw.rect(window,(100, 100, 100),(250, 330, 490,250))
+    draw.rect(window,(140, 72, 4),(375, 200, 365,130))
+    draw.rect(window,(41, 22, 1),(325,370,100,210))
+    draw.circle(window, (0,0,0), (350, 485),8)
+    draw.rect(window,(4, 191, 182),(500, 385, 200, 100))
 
     #Desenhar qualquer poligono:
     draw.polygon(window,(140, 72, 4),((250,330),(375,200),(500,330)))
@@ -42,12 +60,31 @@ while True:
     #Sol
     draw.line(window,(255, 196, 0),(160,120),(250,200),8)
     draw.line(window,(255, 196, 0),(160,120),(100,230),8)
-    draw.line(window,(255, 196, 0),(160,120),(170,230),8)
-    draw.line(window,(255, 196, 0),(160,120),(230,290),8)
-    
-    
-    
+    draw.line(window,(255, 196, 0),(160,120),(180,230),8)
+    draw.line(window,(255, 196, 0),(160,120),(230,260),8)
+    draw.line(window,(255, 196, 0),(160,120),(230,10),8)
+    draw.line(window,(255, 196, 0),(160,120),(250,50),8)
+    draw.line(window,(255, 196, 0),(160,120),(270,105),8)
+    draw.line(window,(255, 196, 0),(160,120),(290,145),8)
+    draw.line(window,(255, 196, 0),(160,120),(25,185),8)
+    draw.line(window,(255, 196, 0),(160,120),(33,120),8)
+    draw.line(window,(255, 196, 0),(160,120),(25,60),8)
+    draw.line(window,(255, 196, 0),(160,120),(45,10),8)
+    draw.line(window,(255, 196, 0),(160,120),(120,10),8)
+    draw.line(window,(255, 196, 0),(160,120),(180,10),8)
     draw.circle(window,(255, 196, 0),(160,120),50)
+
+    #nuvem parada
+    # draw.circle(window,(255, 255, 255,), (800, 100), 50)
+    # draw.circle(window,(255, 255, 255,), (865, 100), 50)
+    # draw.circle(window,(255, 255, 255,), (930, 100), 50)
+    # draw.circle(window,(255, 255, 255,), (995, 100), 50)
+
+    #nuvem andando
+    draw.circle(window,(255, 255, 255), (nuvem_x, 100), 50)
+    draw.circle(window,(255, 255, 255), (nuvem_x + 65, 100), 50)
+    draw.circle(window,(255, 255, 255), (nuvem_x + 130, 100), 50)
+    draw.circle(window,(255, 255, 255), (nuvem_x + 195, 100), 50)
 
     #Arvore
     draw.rect(window,(99, 60, 15),(1000, 420, 50,160))
@@ -55,14 +92,14 @@ while True:
     
     #Desenhar imagem:
     window.blit(newton_img,(740,400))
+    window.blit(maca_img,(1010, 390))
+    window.blit(maca_img,(950, 320))
+    window.blit(maca_img,(1050, 310))
     
     #Desenhar texto:
-    newton_text = newton_font.render("I am Newton", True, (0,0,0))
     window.blit(newton_text,(750, 650))
 
-
     #Colocar som:
-    
-
+    music.play()
     
     display.update()
